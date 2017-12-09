fb81d764b4c62b48b619039b1b190891eb908b2b
Bear Hagen <b@datahjelpen.no>
2017-12-09 18:48:21 +0100

    Added settings for calendar. User can now change year and visuals

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	var cals = document.querySelectorAll('.calendar');
 	var dateYear = getURLParameter('year') || (new Date()).getFullYear();
 	var calInfo = [];
-	var calWeekdaysLong = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
-	                       'Friday', 'Saturday', 'Sunday'];
-	var calWeekdaysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+	var calWeekdaysLong   = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+	                         'Friday', 'Saturday', 'Sunday'];
+	var calWeekdaysShort  = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+	var calWeekdaysNarrow = ['Mo',  'Tu',  'We',  'Th',  'Fr',  'Sa',  'Su'];
 
 	// Turn digit into hex or binary
-	function toHex(d)    { return ("0"+(Number(d).toString(16))).slice(-2).toUpperCase() }
+	function toHex(d)    { return ("0"+(Number(d).toString(16))).slice(-2) }
 	function toBinary(d) { return String("00000" + (+d).toString(2)).slice(-5); }
 
 	// Fill the calInfo variable with info about this year
@@ -27,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		calInfo[mm]['name_long']   = month.toLocaleDateString('en', { month: 'long' });
 		calInfo[mm]['name_short']  = month.toLocaleDateString('en', { month: 'short' });
 		calInfo[mm]['name_narrow'] = month.toLocaleDateString('en', { month: 'narrow' });
+
+		calInfo[mm]['hex']         = toHex(calInfo[mm]['numeric']);
+		calInfo[mm]['binary']      = toBinary(calInfo[mm]['numeric']);
 
 		for (var i = 0; i < calWeekdaysLong.length; i++) {
 			calInfo[mm]['daysByName'][calWeekdaysLong[i]] = [];
@@ -52,10 +56,19 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Put info into calendars
 	for (var i = 0; i < cals.length; i++) {
 
+		cals[i].classList += ' ' + (getURLParameter('paper_size') || 'a4');
+		cals[i].classList += ' ' + (getURLParameter('layout') || 'layout-middle-hole');
+
 		// Create 3 extra months. This allows us to change layout easier
 		for (var extraMonth = 1; extraMonth <= 3; extraMonth++) {
 			var container = document.createElement("div");
 			container.classList += 'cal-container cal-month cal-month-extra cal-month-extra-' + extraMonth;
+
+			// var calYear = document.createElement("h1");
+			// calMonth.classList += 'cal-month-title';
+			// var calMonthName = document.createTextNode(getURLParameter('month_rep') || 'hex');
+
+
 			cals[i].appendChild(container);
 		}
 
@@ -65,10 +78,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
 			firstHasBeen = false;
 
-			// Create weekday names
+			// Create month heading and weekday names
 			(function() {
-				var calMonth = document.createElement("h1");
-				var calMonthName = document.createTextNode(mm);
+				console.log(calInfo[mm]['name_long']);
+
+				var calMonth = document.createElement("h2");
+				calMonth.classList += 'cal-month-title';
+				var calMonthName = document.createTextNode(calInfo[mm][ (getURLParameter('month_rep') || 'hex') ].toUpperCase());
+
 				calMonth.appendChild(calMonthName);
 				container.appendChild(calMonth);
 
@@ -77,8 +94,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
 				for (var c = 0; c < calWeekdaysShort.length; c++) {
 					var heading = document.createElement("li");
-					var dayName = document.createTextNode(calWeekdaysShort[c]);
 
+					switch (getURLParameter('weekday_rep') || 'narrow') {
+						case 'long':
+							weekdayName = calWeekdaysLong[c];
+							break;
+						case 'short':
+							weekdayName = calWeekdaysShort[c];
+							break;
+						case 'narrow':
+							weekdayName = calWeekdaysNarrow[c];
+							break;
+						default:
+							weekdayName = calWeekdaysNarrow[c];
+					}
+
+					var dayName = document.createTextNode(weekdayName);
 					heading.classList += 'cal-heading-' + calWeekdaysLong[c].toLowerCase();
 					heading.appendChild(dayName);
 					column.appendChild(heading);
@@ -94,31 +125,34 @@ document.addEventListener("DOMContentLoaded", function() {
 				for (var c = 0; c < calWeekdaysShort.length; c++) {
 					// Get array with days (in digits) on this day (by name)
 					// For example, get the digits of all Mondays in this month...
-					var daysOnThisDay = calInfo[mm]['daysByName'][calWeekdaysLong[c]];
+					var daysOnThisWeekday = calInfo[mm]['daysByName'][calWeekdaysLong[c]];
+					var dayToShow = daysOnThisWeekday[r];
 					var row = document.createElement("li");
 					row.classList += 'cal-day-' + calWeekdaysLong[c].toLowerCase();
 
 					// First day of the month
-					if (daysOnThisDay[r] == 1) {
+					if (daysOnThisWeekday[r] == 1) {
 						firstHasBeen = true;
 					}
 
 					// The first day of the month have not been yet
 					// lets move this day down to the next row then.
 					if (!firstHasBeen && r == 0 && i == 0) {
-						daysOnThisDay.unshift(0);
+						daysOnThisWeekday.unshift(0);
 					}
 
 					// If this day doesn't exsist (i.e. the 5th sunday in a month with only 4 sundays)
 					// make an empty day, so things align properly
-					if (!daysOnThisDay[r]) {
-						daysOnThisDay[r] = '';
+					if (!daysOnThisWeekday[r]) {
+						dayToShow = '';
 						row.classList += ' cal-day-empty';
 					} else {
-						row.classList += ' cal-day-' + daysOnThisDay[r];
+						row.classList += ' cal-day-' + daysOnThisWeekday[r];
+
+						dayToShow = calInfo[mm]['days'][daysOnThisWeekday[r]][ (getURLParameter('day_rep') || 'hex') ];
 					}
 
-					var dayOnThisDay = document.createTextNode(daysOnThisDay[r]);
+					var dayOnThisDay = document.createTextNode(dayToShow);
 					row.appendChild(dayOnThisDay);
 					column.appendChild(row);
 				}
