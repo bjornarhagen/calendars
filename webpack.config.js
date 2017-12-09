3cccb946ead118a8cd2b978b00c83cf1f449f6bd
Bear Hagen <b@datahjelpen.no>
2017-12-09 17:15:27 +0100

    Setup for project. Added package.json and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({ // define where to save the file
+	filename: './dist/css/[name].css',
+	allChunks: true,
+});
+
+module.exports = {
+	entry: ['./app.js', './src/scss/calendars.scss'],
+	output: {
+		filename: './dist/js/app.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sass|scss)$/,
+					use: extractSass.extract({
+					use: [{
+						loader: "css-loader"
+					}, {
+						loader: "sass-loader"
+					}],
+					// use style-loader in development
+					fallback: "style-loader"
+				})
+			}
+		]
+	},
+	plugins: [
+		extractSass,
+		new CopyWebpackPlugin([
+			{ from: './src/html/2018.html',                    to: __dirname+'/dist/2018.html' },
+		])
+	]
+}
